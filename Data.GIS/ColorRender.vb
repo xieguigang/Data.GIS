536a95ab3e1a9f9c1d01752faad96795539c1367
xieguigang <xie.guigang@gmail.com>
2016-09-23 23:29:03 +0800

    get country

--- Data.GIS/ColorRender.vb
+++ Data.GIS/ColorRender.vb
@@ -3,6 +3,7 @@ Imports Microsoft.VisualBasic.Imaging.SVG
 Imports Microsoft.VisualBasic.SoftwareToolkits
 Imports Microsoft.VisualBasic.Data.csv
 Imports Microsoft.VisualBasic.Linq
+Imports Microsoft.VisualBasic.Language
 
 Public Module ColorRender
 
@@ -36,5 +37,45 @@ Public Module ColorRender
     Public Function Rendering(data As IEnumerable(Of Data)) As SVGXml
         Dim empty As SVGXml = __worldMap
 
+        For Each state As Data In data
+            Dim c As g = empty.__country(state.state)
+        Next
+
+        Return empty
+    End Function
+
+    ''' <summary>
+    ''' thanks to the XML/HTML style of the SVG (and Nathan’s explanation) I can create CSS classes per country 
+    ''' (the polygons of each country uses the alpha-2 country code as a class id)
+    ''' </summary>
+    ''' <param name="map"></param>
+    ''' <param name="code"></param>
+    ''' <returns></returns>
+    <Extension>
+    Private Function __country(map As SVGXml, code As String) As g
+        Dim c As g = map.gs.__country(alpha2:=statDict(code))
+
+        If c Is Nothing Then
+            Throw New NullReferenceException($"Unable found object named '{code}'!")
+        Else
+            Return c
+        End If
+    End Function
+
+    <Extension>
+    Private Function __country(subs As g(), alpha2 As String) As g
+        Dim state As New Value(Of g)
+
+        For Each c As g In subs
+            If String.Equals(alpha2, c.[class], StringComparison.OrdinalIgnoreCase) Then
+                Return c
+            End If
+
+            If Not (state = c.gs.__country(alpha2)) Is Nothing Then
+                Return state
+            End If
+        Next
+
+        Return Nothing
     End Function
 End Module
