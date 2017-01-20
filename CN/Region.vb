8b4982fe605d9a6a49bbf7a27a4266079b46e9f1
xieguigang <xie.guigang@gmail.com>
2017-01-20 22:28:47 +0800

    update sciBASIC package

---
+++ CN/Region.vb
@@ -0,0 +1,13 @@
+﻿Imports Microsoft.VisualBasic.Serialization.JSON
+
+Public Class Region
+
+    Public Property Code As String
+    Public Property Province As String
+    Public Property CityName As String
+    Public Property District As String
+
+    Public Overrides Function ToString() As String
+        Return Me.GetJson
+    End Function
+End Class
