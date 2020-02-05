f69e2e8e6390992c9323039ab196bf98b85f8866
xieguigang <xie.guigang@gcmodeller.org>
2020-02-05 12:58:23 +0800

    render svg

--- Data.GIS/geojson/GeoMapRender.vb
+++ Data.GIS/geojson/GeoMapRender.vb
@@ -1,9 +1,42 @@
-﻿Namespace GeoMap
+﻿Imports Microsoft.VisualBasic.Data.GIS.GeoMap.geojson
+Imports Microsoft.VisualBasic.Imaging
+Imports Microsoft.VisualBasic.Imaging.Drawing2D
+Imports Microsoft.VisualBasic.Imaging.Driver
+Imports Microsoft.VisualBasic.MIME.application
+Imports Microsoft.VisualBasic.MIME.application.json
+Imports Microsoft.VisualBasic.Scripting.Runtime
+
+Namespace GeoMap
 
     ''' <summary>
     ''' Convert geojson data to svg map model
     ''' </summary>
     Public Class GeoMapRender
 
+        Private Sub New()
+        End Sub
+
+        Public Sub Plot(ByRef g As IGraphics, layout As GraphicsRegion)
+
+        End Sub
+
+        Public Shared Function Render(geo As String) As SVGData
+            Dim geoJson As FeatureCollection = json _
+                .ParseJson(JsonStr:=geo.SolveStream) _
+                .CreateObject(GetType(FeatureCollection))
+            Dim svg As SVGData = GeoMapRender.Render(geoJson)
+
+            Return svg
+        End Function
+
+        Public Shared Function Render(geo As FeatureCollection, Optional size$ = "5000,3000") As SVGData
+            Return g.GraphicsPlots(
+                size:=size.SizeParser,
+                padding:=g.ZeroPadding,
+                bg:="white",
+                plotAPI:=AddressOf New GeoMapRender().Plot,
+                driver:=Drivers.SVG
+            )
+        End Function
     End Class
 End Namespace
--- Data.GIS/test/geojsonReader.vb
+++ Data.GIS/test/geojsonReader.vb
@@ -1,13 +1,24 @@
-﻿Imports Microsoft.VisualBasic.Data.GIS.GeoMap.geojson
+﻿Imports Microsoft.VisualBasic.Data.GIS.GeoMap
+Imports Microsoft.VisualBasic.Data.GIS.GeoMap.geojson
 Imports Microsoft.VisualBasic.MIME.application
 Imports Microsoft.VisualBasic.MIME.application.json
 
 Module geojsonReader
 
     Sub Main()
+        Call renderSVG("E:\Data.GIS\geojson-map-china\geometryCouties\140300.json")
+
+        Pause()
+    End Sub
+
+    Sub loadTest()
         Dim geo = json.ParseJsonFile("E:\Data.GIS\geojson-map-china\geometryCouties\140300.json").CreateObject(GetType(FeatureCollection))
 
         Pause()
+    End Sub
+
+    Sub renderSVG(file$)
+        Call GeoMapRender.Render("E:\Data.GIS\geojson-map-china\geometryCouties\140300.json").Save("./test.svg")
     End Sub
 
 End Module
