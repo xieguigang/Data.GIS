f4a5b813a6f29afc5c8d43ccc1483d0de6602e2f
xieguigang <xie.guigang@gcmodeller.org>
2020-02-05 09:52:21 +0800

    found bugs in json object loader

--- Data.GIS/test/geojsonReader.vb
+++ Data.GIS/test/geojsonReader.vb
@@ -1,7 +1,13 @@
-﻿Module geojsonReader
+﻿Imports Microsoft.VisualBasic.Data.GIS.geojson
+Imports Microsoft.VisualBasic.MIME.application
+Imports Microsoft.VisualBasic.MIME.application.json
+
+Module geojsonReader
 
     Sub Main()
+        Dim geo = json.ParseJsonFile("E:\Data.GIS\geojson-map-china\geometryCouties\140300.json").CreateObject(GetType(FeatureCollection))
 
+        Pause()
     End Sub
 
 End Module
