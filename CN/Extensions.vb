707840f1715d4c0dcfff74830239816b1e64de91
xieguigang <xie.guigang@gcmodeller.org>
2020-02-18 11:19:51 +0800

    add geo china json resource

--- CN/Extensions.vb
+++ CN/Extensions.vb
@@ -1,4 +1,9 @@
 ﻿Imports System.Runtime.CompilerServices
+Imports System.Text
+Imports Microsoft.VisualBasic.Data.GIS.GeoMap.geojson
+Imports Microsoft.VisualBasic.Linq
+Imports Microsoft.VisualBasic.MIME.application
+Imports Microsoft.VisualBasic.MIME.application.json
 
 Public Module Extensions
 
@@ -11,5 +16,15 @@ Public Module Extensions
         Next
 
         Return Nothing
+    End Function
+
+    Public Function GeoChina() As FeatureCollection
+        Dim resJson$ = My.Resources _
+            .__china_geo _
+            .DoCall(AddressOf Encoding.UTF8.GetString)
+
+        Return json _
+            .ParseJson(JsonStr:=resJson) _
+            .CreateObject(GetType(FeatureCollection))
     End Function
 End Module
--- CN/My Project/Resources.Designer.vb
+++ CN/My Project/Resources.Designer.vb
@@ -22,7 +22,7 @@ Namespace My.Resources
     '''<summary>
     '''  A strongly-typed resource class, for looking up localized strings, etc.
     '''</summary>
-    <Global.System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "15.0.0.0"),  _
+    <Global.System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0"),  _
      Global.System.Diagnostics.DebuggerNonUserCodeAttribute(),  _
      Global.System.Runtime.CompilerServices.CompilerGeneratedAttribute(),  _
      Global.Microsoft.VisualBasic.HideModuleNameAttribute()>  _
@@ -58,6 +58,16 @@ Namespace My.Resources
             Set
                 resourceCulture = value
             End Set
+        End Property
+        
+        '''<summary>
+        '''  Looks up a localized resource of type System.Byte[].
+        '''</summary>
+        Friend ReadOnly Property __china_geo() As Byte()
+            Get
+                Dim obj As Object = ResourceManager.GetObject("__china_geo", resourceCulture)
+                Return CType(obj,Byte())
+            End Get
         End Property
         
         '''<summary>
